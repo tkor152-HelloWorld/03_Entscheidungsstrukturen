966e4ed38de8173026eadde3f19147656b055ea9
git4cc-noob <ktcc.0822@gmail.com>
2023-01-16 14:45:47 +0100

    Fallunterscheidung / SWITCH|CASE 2

--- script_01.js
+++ script_01.js
@@ -14,11 +14,11 @@ isJohnOlder = (ageJohn > ageMark);
 isJohnEqual = (ageJohn == ageMark);
 
 // Ausgabe
-console.log("ageJohn: " + ageJohn);
-console.log("ageMark: " + ageMark);
-console.log("isJohnOlder: " + isJohnOlder);
-console.log("isJohnEqual: " + isJohnEqual);
-console.log("------------------");
+// console.log("ageJohn: " + ageJohn);
+// console.log("ageMark: " + ageMark);
+// console.log("isJohnOlder: " + isJohnOlder);
+// console.log("isJohnEqual: " + isJohnEqual);
+// console.log("------------------");
 
 /************ IF  ************/
 // TINA --> There is no alternative!
@@ -53,18 +53,72 @@ console.log("------------------");
 /************ IF - ELSE IF  ************/
 // mit alternativen Fällen (älter, jünger, gleich alt)
 
-if (isJohnOlder) 
-{
-    console.log("John ist älter.")
-}
-// < --- 1. Alernative
-else if (isJohnEqual)
-{
-    console.log("John und Mark sind gleich alt.")
-}
-// < --- 2.Alternative
+// if (isJohnOlder) 
+// {
+//     console.log("John ist älter.")
+// }
+// // < --- 1. Alernative
+// else if (isJohnEqual)
+// {
+//     console.log("John und Mark sind gleich alt.")
+// }
+// // < --- 2.Alternative
 
-else 
+// else 
+// {
+//     console.log("John ist jünger.")
+// }
+
+/****** Fallunterscheidung / SWITCH|CASE 1 ******/
+
+ 
+
+const firstName = "Jane";
+let job;
+
+// job = "driver";     // .. fährt TAXI! / UBER
+// job = "diver";      // .. taucht im Rhein! 
+// job = "artist";     // .. malt ein Bild!
+// job = "pilot";      // .. macht etwas anderes! --> default
+// job = "teacher";    // .. unterrichtet!
+// job = "instructor";  // .. unterrichtet!
+
+// switch (job)
+//  {
+//     case "driver":
+//         console.log("Jane fährt Taxi!");
+//         break;
+//     case "diver":
+//         console.log("Jane taucht im Rhein!");
+//         break;
+//     case "artist":
+//         console.log("Jane malt ein Bild!");
+//         break;
+//     case "teacher":
+//     case "instructor":    
+//        console.log("Jane unterrichtet!");
+//        break;
+    // case "teacher" || "instructor":    
+    //     console.log("Jane unterrichtet!");
+    //     break;   
+//     default:
+//         console.log("Jane macht etwas anderes");
+//         break;
+// }
+/****** Fallunterscheidung / SWITCH|CASE 2 ******/
+
+const cond = true;
+const a = 10;
+
+switch (cond)
 {
-    console.log("John ist jünger.")
+    case (a == 1):
+        console.log("a ist gleich 1");
+        break;
+    case (a == 2):
+        console.log("a ist gleich 2");
+        break;
+    default:
+        console.log("a hat einen anderen Wert");
+        break;        
 }
