6cfa1f493923153f0ccdcbce4f0a7f6c361b2c9e
git4cc-noob <ktcc.0822@gmail.com>
2023-01-16 12:21:50 +0100

    1.IF

--- script_01.js
+++ script_01.js
@@ -9,3 +9,25 @@ const ageMark = 30;
 // Deklaration
 let isJohnOlder, isJohnEqual;
 
+// Test | Logische Aussage
+isJohnOlder = (ageJohn > ageMark);
+isJohnEqual = (ageJohn == ageMark);
+
+// Ausgabe
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
+console.log("------------------");
+
+/************ IF  ************/
+// TINA --> There is no alternative!
+// entweder JA oder nix ... alternativlos
+
+// if (true)
+// if (isJohnOlder)
+if (ageJohn > ageMark)
+{
+    console.log("John ist älter.")
+}
+
